8260492a26307d12bae90929b24717be04199bf3
Rhythm Bhandari <rhythmbhandari@outlook.com>
2022-11-15 11:58:00 +0545

    Boiler code for Bloc

---
+++ android/app/src/main/kotlin/com/example/bloc_boiler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_boiler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,44 @@
+import '../app/app_prefs.dart';
+import '../app/di.dart';
+import '../presentation/resources/routes_manager.dart';
+import '../presentation/resources/theme_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import 'app_prefs.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal(); // private named constructor
+  int appState = 0;
+  static final MyApp instance =
+  MyApp._internal(); // single instance -- singleton
+
+  factory MyApp() => instance; // factory for the class instance
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocal().then((local) => {context.setLocale(local)});
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
+      theme: getApplicationTheme(),
+    );
+  }
+}
